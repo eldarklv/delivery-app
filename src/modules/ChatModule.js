d084847f3b82dfe32c239b709b19e1acdb3cada2
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2024-01-15 02:27:57 +0300

    do find method

--- src/modules/ChatModule.js
+++ src/modules/ChatModule.js
@@ -22,20 +22,38 @@ const ChatModule = {
 
     return message;
   },
+
+  async find(users) {
+    const user0 = users[0]
+    const user1 = users[1]
+
+    const chat = await Chat.findOne({ users: { $all: [user0, user1] } });
+
+    return chat;
+  },
 };
 
-async function testSendMsg() {
-  try {
-    const message = await ChatModule.sendMessage({
-      author: "65a41cd8d9e70b266bb2957a",
-      receiver: "65a43b41372ea956b442de07",
-      text: "hello world",
-    });
-    console.log(message);
-  } catch (error) {
-    console.error(error);
-  }
+// async function testSendMsg() {
+//   try {
+//     const message = await ChatModule.sendMessage({
+//       author: "65a41cd8d9e70b266bb2957a",
+//       receiver: "65a43b41372ea956b442de07",
+//       text: "hello world",
+//     });
+//     console.log(message);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// testSendMsg();
+
+async function testFind() {
+  const chat = await ChatModule.find([
+    "65a41cd8d9e70b266bb2957a",
+    "65a43b41372ea956b442de07",
+  ]);
+  console.log(chat)
 }
-testSendMsg();
+testFind()
 
 module.exports = ChatModule;
