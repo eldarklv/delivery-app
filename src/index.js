6b9276e9427ca9ac1bd477b6af9669233a91467e
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2024-01-16 01:06:04 +0300

    socket done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/ApiRoutes");
-const passport = require("passport")
+const passport = require("passport");
 const session = require("express-session");
+const ChatModule = require("./modules/ChatModule");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const UserModule = require("./modules/UserModule");
+
 require("dotenv").config();
 require("./config/passportConf");
-require("./modules/ChatModule")
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(
   session({
     secret: "secret",
@@ -20,14 +26,27 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use("/api", userRouter);
 
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
-console.log("LOOK", MONGO_URL);
+
+io.on("connection", (socket) => {
+  console.log(socket.handshake);
+
+  socket.on("getHistory", async (user0, user1) => {
+    const chat = await ChatModule.find([user0, user1]);
+    const { messages } = chat;
+    socket.emit("chatHistory", messages);
+  });
+
+  socket.on("sendMessage", async (author, receiver, text) => {
+    const message = await ChatModule.sendMessage({ author, receiver, text });
+    socket.emit("newMessage", message)
+  });
+});
 
 mongoose.connect(MONGO_URL).then(() => console.log("MongoDB connected"));
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
