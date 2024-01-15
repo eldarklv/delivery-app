86e5488ccd21cd702cd9b3c38a787e622b48d168
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2024-01-15 22:42:27 +0300

    do event emitter

--- src/modules/ChatModule.js
+++ src/modules/ChatModule.js
@@ -1,5 +1,8 @@
 const Chat = require("../models/ChatModel");
 const Message = require("../models/MessageModel");
+const { EventEmitter } = require("events");
+
+const eventEmitter = new EventEmitter();
 
 const ChatModule = {
   async sendMessage(data) {
@@ -20,40 +23,49 @@ const ChatModule = {
     chat.messages.push(message);
     chat.save();
 
+    eventEmitter.emit("newMessage", { chatId: chat._id, message });
+
     return message;
   },
 
   async find(users) {
-    const user0 = users[0]
-    const user1 = users[1]
+    const user0 = users[0];
+    const user1 = users[1];
 
     const chat = await Chat.findOne({ users: { $all: [user0, user1] } });
 
     return chat;
   },
+
+  subscribe(callback) {
+    eventEmitter.on("newMessage", callback);
+  },
 };
 
 // async function testSendMsg() {
 //   try {
+//     ChatModule.subscribe((data) => {
+//       console.log("LOOK", data.chatId, data.message);
+//     });
+
 //     const message = await ChatModule.sendMessage({
 //       author: "65a41cd8d9e70b266bb2957a",
 //       receiver: "65a43b41372ea956b442de07",
 //       text: "hello world",
 //     });
-//     console.log(message);
 //   } catch (error) {
 //     console.error(error);
 //   }
 // }
 // testSendMsg();
 
-async function testFind() {
-  const chat = await ChatModule.find([
-    "65a41cd8d9e70b266bb2957a",
-    "65a43b41372ea956b442de07",
-  ]);
-  console.log(chat)
-}
-testFind()
+// async function testFind() {
+//   const chat = await ChatModule.find([
+//     "65a41cd8d9e70b266bb2957a",
+//     "65a43b41372ea956b442de07",
+//   ]);
+//   console.log(chat);
+// }
+// testFind();
 
 module.exports = ChatModule;
